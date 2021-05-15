7ee2eadbf1c019fd6177dc9755b39faa1206398b
nbr41to <nbr.41to@gmail.com>
2021-05-15 17:40:39 +0900

    できた

--- pages/api/post.js
+++ pages/api/post.js
@@ -2,15 +2,27 @@ const { Client } = require('@notionhq/client');
 
 const notion = new Client({ auth: process.env.INTEGRATION_TOKEN });
 
-export default async (req, res) => {
+export default async () => {
   try {
-    console.log(process.env.INTEGRATION_TOKEN);
-    console.log(process.env.DATABASE_ID);
-    const pageId = process.env.DATABASE_ID;
-    const response = await notion.pages.retrieve({ page_id: pageId });
+    const response = await notion.pages.create({
+      parent: {
+        database_id: process.env.DATABASE_ID,
+      },
+      properties: {
+        Name: {
+          title: [
+            {
+              text: {
+                content: "new title yeah!!"
+              }
+            }
+          ]
+        }
+      }
+    });
     console.log(response);
+    return response;
   } catch (error) {
     return error;
   }
-  return response;
 };
--- pages/index.jsx
+++ pages/index.jsx
@@ -2,49 +2,23 @@ import axios from 'axios';
 // import fetch from 'node-fetch';
 
 export default function Home() {
-  // axios.get("https:/localhost:3000/api/hello").then(a => console.log(a.data));
+  // axios.get("/api/hello").then(a => console.log(a.data));
 
-  // axios.get("/api/post").then(a => console.log(a));
-  const post = () => {
-    try {
-      // fetch('https://api.notion.com/v1/pages/b55c9c91-384d-452b-81db-d1ef79372b75', {
-      //   mode: 'no-cors',
-      //   headers: {
-      //     'Notion-Version': '2021-05-13',
-      //     'Authorization': 'Bearer secret_GgXF30iqZ1SjuacFgNLltGzdoRGXBK7p0munoylhnSa'
-      //   }
-      // });
-      fetch('https://api.notion.com/v1/pages', {
-        mode: 'no-cors',
-        method: 'POST',
-        headers: {
-          'Authorization': 'Bearer secret_GgXF30iqZ1SjuacFgNLltGzdoRGXBK7p0munoylhnSa',
-          'Content-Type': 'application/json',
-          'Notion-Version': '2021-05-13'
-        },
-        body: JSON.stringify({ "parent": { "database_id": "481be50a529340e5856a801280ec6e8c" }, "properties": { "Name": { "title": [{ "text": { "content": "追加されたタイトル Yeah!!" } }] } } })
-      });
-
-      // var fetch = require('node-fetch');
-      // fetch('https://api.notion.com/v1/pages', {
-      //   mode: 'no-cors',
-      //   method: 'POST',
-      //   headers: {
-      //     'Authorization': 'Bearer secret_GgXF30iqZ1SjuacFgNLltGzdoRGXBK7p0munoylhnSa',
-      //     'Content-Type': 'application/json',
-      //     'Notion-Version': '2021-05-13'
-      //   },
-      //   body: JSON.stringify({ "parent": { "database_id": "481be50a529340e5856a801280ec6e8c" }, "properties": { "Name": { "title": [{ "text": { "content": "追加されたタイトル Yeah!!" } }] } } })
-      // });
-    } catch (error) {
-      console.log(error);
-    }
+  const testPost = () => {
+    // axios.post("/api/post").then(a => console.log(a));
+    fetch("/api/post").then(a => console.log("testpost", a));
+  };
+  const testGet = () => {
+    // axios.post("/api/post").then(a => console.log(a));
+    fetch("/api/post", {
+      method: "POST",
+    }).then(a => console.log(a));
   };
   return (
     <div>
       <h1>Lesson Notion API</h1>
-      <button onClick={post}>post</button>
-      <button onClick={post}>get</button>
+      <button onClick={testPost}>post</button>
+      {/* <button onClick={post}>get</button> */}
     </div>
   );
 }
